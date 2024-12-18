32e0caafbafb8568f6a3c885925962ad522500a6
moehike <59832236+scarlettjclark@users.noreply.github.com>
2024-12-18 15:03:37 +0000

    mana card added

---
+++ card.gd
@@ -0,0 +1,9 @@
+extends Area3D
+
+func _on_input_event(camera, event, event_position, normal, shape_idx):
+	var mouse_click = event as InputEventMouseButton
+	if mouse_click and mouse_click.button_index == 1 and mouse_click.pressed:
+		play()
+
+func play():
+	pass
---
+++ mana_card.gd
@@ -0,0 +1,7 @@
+extends "card.gd"
+
+@export
+var mana_tracker : Label3D;
+
+func play():
+	mana_tracker.text = str(int(mana_tracker.text) + 1)
