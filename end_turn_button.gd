b722719afc1547273c8ed8e17b6eb4a92d28030f
moehike <59832236+scarlettjclark@users.noreply.github.com>
2024-12-18 17:09:11 +0000

    end turn now instantiates cards in correct location

--- end_turn_button.gd
+++ end_turn_button.gd
@@ -2,7 +2,12 @@ extends Area3D
 var mana_card = preload("res://mana_card.tscn")
 var slug = preload("res://slug.tscn")
 
-var example_hand = [slug, slug, mana_card, mana_card]
+var example_hand = [slug, slug, mana_card, mana_card, mana_card]
+var card_positions = [Vector3(-0.5, 0, 0), 
+						Vector3(-0.25, 0, 0),
+						Vector3(0,0,0),
+						Vector3(0.25, 0, 0),
+						Vector3(0.5,0,0)]
 
 func _on_input_event(camera, event, event_position, normal, shape_idx):
 	var mouse_click = event as InputEventMouseButton
@@ -11,6 +16,7 @@ func _on_input_event(camera, event, event_position, normal, shape_idx):
 		for card in hand.get_children():
 			card.queue_free()
 		
-		for card in example_hand:
-			var card_instance = card.instantiate()
+		for i in range(5):
+			var card_instance = example_hand[i].instantiate()
 			hand.add_child(card_instance)
+			card_instance.position = card_positions[i]
